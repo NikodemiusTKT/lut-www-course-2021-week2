77e3e07b5de30e664a6b9ddb3da028b3147c295e
Teemu Tanninen <teemuxtanninen@gmail.com>
2021-09-14 17:14:55 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+document.getElementById("add-comment").onclick = () => {
+  var comment = document.getElementById("comment-text");
+  var ul = document.getElementById("comment-list");
+  var commentText = comment.value;
+  if (commentText === "") return;
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(commentText));
+  ul.appendChild(li);
+  comment.value = "";
+};
+document.getElementById("remove-comments").onclick = () => {
+  var doRemoveComments = window.confirm(
+    "Are you sure you want to remove comments?"
+  );
+  if (doRemoveComments) {
+    document.getElementById("comment-list").innerHTML = "";
+  } else {
+    return;
+  }
+};
