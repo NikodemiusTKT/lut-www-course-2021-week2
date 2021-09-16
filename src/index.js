384a5ca63bd41b96584a05eb0b0fb31607e2faf6
Teemu Tanninen <teemu.tanninen@student.lut.fi>
2021-09-16 17:57:39 +0300

    Simplified comment template and improved CSS.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import './styles.css';
 document.getElementById('add-comment').onclick = () => {
   var commentText = document.getElementById('comment-input');
-  var ratingValue = document.getElementById('select-rating');
+  var rating = document.getElementById('select-rating');
   var commentList = document.getElementById('comment-list');
-  var ratingTemplate = buildRatingStars(ratingValue.value);
   var template = `
 			<div class="comment-rating">
-				${ratingTemplate}
+				${rating.value}
 			</div>
 			<div class="comment-text">${commentText.value}</div>
+
 		`;
   var div = document.createElement('div');
   div.classList.add('comment');
@@ -31,21 +31,3 @@ document.getElementById('remove-comments').onclick = () => {
     buttons[index].style.visibility = 'visible';
   }
 };
-function buildRatingStars(rating) {
-  var ratingTemplate = ``;
-  let stars = 4;
-  for (let index = 0; index < rating; index++) {
-    ratingTemplate += `<span class="fa fa-star checked"></span>`;
-    stars -= 1;
-  }
-  for (let index = 0; index < stars; index++) {
-    ratingTemplate += `<span class="fa fa-star"></span>`;
-  }
-  return ratingTemplate;
-}
-// function removeComment(event) {
-//   var dom = this;
-//   var p_dom = this.parentNode;
-//   var parent_node = p_dom.parentNode;
-//   parent_node.removeChild(p_dom);
-// }
